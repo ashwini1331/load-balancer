6e43860323f8a34788d22fa34bb8ce233576ce71
ashwini <53709167+ashwini1331@users.noreply.github.com>
2020-09-06 08:28:30 +0530

    Update Web-server.sh

--- Web-server.sh
+++ Web-server.sh
@@ -11,4 +11,5 @@ sudo chgrp -R tomcat /opt/tomcat
 sudo chmod -R g+r conf
 sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
+sudo update-java-alternatives -l
 sudo nano /etc/systemd/system/tomcat.service
