b823e067476253fdc4fedd0e830f86313299cf84
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-08-13 08:38:20 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,26 @@
+
+variable "vpc_id" {
+ description = " Attacment of VPC ID here"
+ default = ""
+}
+variable "instance1_id" {
+  description = "Attach 1st Instance id here"
+  default = ""
+
+}
+
+variable "instance2_id" {
+  description = "Attach 2nd Instance id here"
+  default = ""
+}
+
+variable "subnet1" {
+  description = "Attach subnet 1st id here"
+  default = ""
+}
+
+variable "subnet2" {
+  description = "Attach subnet id here"
+  default = ""
+
+}
