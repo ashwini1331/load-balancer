d115f8111873f364857ea2f0ae83d3dfaaff7d85
ashwini <53709167+ashwini1331@users.noreply.github.com>
2022-08-13 08:39:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,12 +39,9 @@ resource "aws_lb" "my-aws-alb" {
   internal = false
   security_groups = ["${aws_security_group.my-alb-sg.id}"]
   subnets = ["${var.subnet1}","${var.subnet2}"]
- 
-
   tags = {
     Name = "my-test-alb"
   }
-
   ip_address_type    = "ipv4"
   load_balancer_type = "application"
 }
